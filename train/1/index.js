b48877ad14b91c5e37d7d99fe00c879b48fc14da
jinglf000 <jinglongfei@jsb5.com>
2018-01-17 18:14:47 +0800

    feat(module):第二课

--- train/1/index.js
+++ train/1/index.js
@@ -1,20 +1,21 @@
 const Koa = require('koa');
 const app = new Koa();
 
+// 第一层洋葱皮 ctx 为context 上下文
 app.use(async (ctx, next) => {
   const start = Date.now();
   await next();
   const ms = Date.now() - start;
   ctx.set('X-Response-Time', `${ms}ms`);
 });
-
+// 第二层洋葱皮
 app.use(async (ctx, next) => {
   const start = Date.now();
   await next();
   const ms = Date.now() - start;
   console.log(`${ctx.method} ${ctx.url} - ${ms}`);
 });
-
+// 第三层洋葱皮
 app.use(async ctx => {
   ctx.body = 'hello World';
 });
---
+++ train/2/index.js
@@ -0,0 +1,21 @@
+const Koa = require('koa');
+const utils = require('utility');
+const logger = require('koa-logger');
+const app = new Koa();
+
+app.use(logger());
+
+app.use((ctx, next) => {
+  if (ctx.path === '/') {
+    const query = ctx.query.q;
+    let md5Val;
+    try {
+      md5Val = utils.md5(query);
+    } catch (err) {
+      console.log(err)      
+    }
+    ctx.body = md5Val;
+  }
+});
+
+app.listen(3000);
