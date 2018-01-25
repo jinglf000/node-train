09751d5dc4819b257db3684f2cd454edefdda90c
jinglf000 <jinglongfei@jsb5.com>
2018-01-25 15:45:45 +0800

    feat(module):学习了beachmark测试

--- train/10/index.js
+++ train/10/index.js
@@ -2,6 +2,7 @@
 
 var Beechmark = require('benchmark');
 var suit = new Beechmark.Suite;
+var chalk = require('chalk');
 
 var init1 = function (str) {
   return +str;
@@ -14,6 +15,25 @@ var init2 = function (str) {
 var init3 = function (str) {
   return Number(str);
 }
-
+let num = 0;
 var number = '100';
 
+suit.add('+', function () {
+    init1(number);
+  })
+  .add('parseInt', () => {
+    init2(number);
+  })
+  .add('Number', () => {
+    init3(number);
+  })
+  .on('cycle', (e) => {
+    const list = ['green', 'red', 'yellow'];
+    console.log(chalk[list[num++]](String(e.target)));
+  })
+  .on('complete', function (e) {
+    console.log(`Fastest is ${this.filter('fastest').map('name')}`);
+  })
+  .run({
+    'async': true
+  });
